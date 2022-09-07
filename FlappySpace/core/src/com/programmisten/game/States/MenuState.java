def5eecce659334653fec795fc52e7af894871a6
Adrian Duda <AdrianDuda2019@gmail.com>
2022-09-07 22:32:10 +0200

    Added assets and fixed gui

--- FlappySpace/core/src/com/programmisten/game/States/MenuState.java
+++ FlappySpace/core/src/com/programmisten/game/States/MenuState.java
@@ -12,6 +12,11 @@ public class MenuState extends State{
     private Texture playBtn;
     private Texture title;
     private Texture player;
+    private Texture settingsButton;
+    private Texture highscoreButton;
+    private int playBtnTextGap;
+    private int buttonScale;
+    private int buttonMargin;
 
     public MenuState(GameStateManager gsm){
         super(gsm);
@@ -19,6 +24,11 @@ public class MenuState extends State{
         playBtn = new Texture("play.png");
         title = new Texture("title.png");
         player = new Texture("vehicle1.png");
+        settingsButton = new Texture("settings1.png");
+        highscoreButton = new Texture("highscore1.png");
+        playBtnTextGap = 100;
+        buttonScale = 50;
+        buttonMargin = 10;
     }
 
 
@@ -31,15 +41,18 @@ public class MenuState extends State{
     public void update(float dt) {
 
     }
-
+    // Hardcoded for aligning :/
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(background, 0,0, FlappySpace.WIDTH, FlappySpace.HEIGHT);
-        sb.draw(playBtn, (FlappySpace.WIDTH / 2) - (playBtn.getWidth()/2), (FlappySpace.HEIGHT / 3));
+        sb.draw(background,0,0, FlappySpace.WIDTH,FlappySpace.HEIGHT);
+        sb.draw(playBtn, (FlappySpace.WIDTH/2)-(playBtn.getWidth()/2),FlappySpace.HEIGHT /2 - playBtnTextGap);
         // We need to scale that..
-        sb.draw(title, (FlappySpace.WIDTH / 2) - (title.getWidth()/2), (FlappySpace.HEIGHT / 2));
-        sb.draw(player, (FlappySpace.WIDTH / 2) - (player.getWidth()/2), (FlappySpace.HEIGHT / 2));
+        sb.draw(title, FlappySpace.WIDTH/4,FlappySpace.HEIGHT /2 + playBtnTextGap, title.getWidth() / 10, title.getHeight()/10);
+
+        sb.draw(player,(FlappySpace.WIDTH/2)-(player.getWidth()/2),FlappySpace.HEIGHT /2);
+        sb.draw(settingsButton, FlappySpace.WIDTH - buttonScale -buttonMargin, FlappySpace.HEIGHT - buttonScale -buttonMargin, buttonScale, buttonScale);
+        sb.draw(highscoreButton, 0+buttonMargin, FlappySpace.HEIGHT - buttonScale -buttonMargin, buttonScale, buttonScale);
         sb.end();
 
     }
